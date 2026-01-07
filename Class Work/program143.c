dfb6479cf493e3ef01ef5dc5e9c7b7c822e9fb24
Anuj <anujpipare8@gmail.com>
2026-01-07 11:06:17 +0530

    .

---
+++ Class Work/program143.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int * IPTR;
+// O(N)
+bool Linearsearch(int Arr[], int iSize, int iNo)
+{
+    int iCnt = 0;
+    bool bFlag = false;
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+   return bFlag;
+}
+int main()
+{
+    int iLength = 0, iCnt = 0,  iValue = 0;
+    bool bRet = false;
+    IPTR iPtr = NULL;
+
+    printf("enter the number of elemnts :\n");
+    scanf("%d",&iLength);
+    // step1 : allocate the memory
+
+    iPtr = (IPTR)malloc(iLength * sizeof(int));
+    if(NULL == iPtr)
+    {
+        printf("unable to allocate the memory\n");
+        return-1;
+    }
+    printf("enter the value : \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&iPtr[iCnt]);
+    }
+    printf("enter the number to search :\n");
+    scanf("%d",iValue);
+
+    // step2 : use the memory
+    bRet = Linearsearch(iPtr, iLength,iValue);
+    if(bRet == true)
+    {
+        printf("%d is present in the data\n",iValue);
+    }
+    else
+    {
+         printf("%d is not present in the data\n",iValue);
+    }
+ 
+    free(iPtr);
+    return 0;
+}
