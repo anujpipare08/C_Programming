71eab95c6bff681d96b97a07c4b7a7f46d1d7947
Anuj <anujpipare8@gmail.com>
2026-01-08 12:15:31 +0530

    .

---
+++ Class Work/program147.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int * IPTR;
+// O(N)
+void Update(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        Arr[iCnt]++;
+    }
+}
+int main()
+{
+    int iLength = 0, iCnt = 0, iRet = 0;
+    IPTR iPtr = NULL;
+
+    printf("enter the number of elemnts :\n");
+    scanf("%d",&iLength);
+    // step1 : allocate the memory
+
+    iPtr = (IPTR)malloc(iLength * sizeof(int));
+    if(NULL == iPtr)
+    {
+        printf("unable to allocate the memory\n");
+        return-1;
+    }
+    printf("enter the value : \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&iPtr[iCnt]);
+    }
+
+    // step2 : use the memory
+     Update(iPtr, iLength);
+
+     printf("updated date from array is :");
+     for(iCnt = 0; iCnt < iLength;)
+ 
+ 
+    free(iPtr);
+    return 0;
+}
+
