6953b83b84f8e3ab03d1d4b555cb04c1ba3e573b
Anuj <anujpipare8@gmail.com>
2026-01-12 23:35:49 +0530

    .

---
+++ Class Work/program156.java
@@ -0,0 +1,30 @@
+// INput : 6
+// Output :654321
+
+import java.util.*;
+class Pattern
+{
+    public void Display(int iNo)
+    {
+        int iCnt = 0;
+        for(iCnt = iNo; iCnt >= 1; iCnt--)
+        {
+          System.out.print(iCnt+"\t");
+        }
+        System.out.println();
+    }
+}
+class program156
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the frequency :");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
