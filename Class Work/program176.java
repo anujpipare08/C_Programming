c8b0289e538030f0bba87e878d76eb0841bed5d1
Anuj <anujpipare8@gmail.com>
2026-01-18 22:19:28 +0530

    .\

---
+++ Class Work/program176.java
@@ -0,0 +1,34 @@
+/*
+   input : 5
+   output : A   B   C   D   E
+   index : 1    2   3   4   5
+   ASCII : 65   66  67  68  69
+*/
+import java.util.*;
+class Pattern
+{
+    public void Display(int iNo)
+    {
+       int iCnt = 0;
+       char ch = 'A';
+       for(iCnt = 1; iCnt <= iNo; iCnt++,ch++)
+       {
+          System.out.printf("%c\t",ch);
+       } 
+       System.out.println();
+    }
+}
+class program176
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the frequemcy :");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
