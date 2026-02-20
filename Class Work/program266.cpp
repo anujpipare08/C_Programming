a6257910df6fd9884100788fcff787236ad7806f
Anuj <anujpipare8@gmail.com>
2026-02-20 17:49:40 +0530

    >

---
+++ Class Work/program266.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+template <class T>
+
+T Add(T No1, T No2)
+{
+    T Ans;
+    Ans = No1 + No2;
+    return Ans;
+
+}
+template < class T>
+T Maximum(T No1, T No2)
+{
+    if(No1 > No2)
+    {
+        return No1;
+    }
+    else
+    {
+        return No2;
+    }
+}
+int main()
+{
+    double dValue1 = 10.0,  dValue2 = 11.0,  dRet = 0.0;
+   
+    dRet = Maximum(dValue1,dValue2);
+    cout<<"Maximum  :"<<dRet<<"\n";
+
+    return 0;
+}
